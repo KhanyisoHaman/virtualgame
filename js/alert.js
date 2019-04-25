4b2a770935f9b185b9090636bc58dfd8c308c2ff
Khanyiso Haman <khanyisohaman@gmail.com>
2019-04-25 15:30:56 +0200

    make an alert function

---
+++ js/alert.js
@@ -0,0 +1,3 @@
+// My Messages to my Potential Employers
+
+alert("Please note: This was one of my first websites that I have built. I left it in this manner for I love my roots . It is not functional nor Responsive");
